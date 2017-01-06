07a8cf0964cd87074b056af95c0e013bd76152e6
Mishyaz <123qweASD>
2017-01-06 12:19:44 +0200

    add ex1 - ex2 create Shuffle1

---
+++ src/ex1/PhraseOMatic.java
@@ -0,0 +1,20 @@
+package ex1;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"круглосуточный", "трех-звенный", "30000-футовый", "взаимный", "обоюдный выигрыш", "фронтенд", "на основе веб-технологий", "проникающий", "умный", "шесть сигм", "метод критического пути", "динамичный"};
+        String[] wordListTwo = {"уполномоченнный", "ориентированный", "центральный", "распределенный", "кластеризованный", "фирменный", "нестандартный ум и", "позиционированный", "сетевой", "сфокусированный", "использованный с выгодой", "выровненный", "нацеленный на", "общий", "совместный", "ускоренный"};
+        String[] wordListThree = {"процесс", "пункт разгрузки", "выход из положения", "тип структуры", "талант", "подход", "уровень завоеванного доверия", "портал", "период времени", "обзор", "образец", "пункт следования"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("Все что вам нужно, - это " + phrase);
+    }
+}
