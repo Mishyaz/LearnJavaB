66f6996bb881ce24e7fa39822751feedc67e4e84
Mishyaz <123qweASD>
2017-01-25 15:41:10 +0200

    add ex15

---
+++ src/ex15SeaBattle/SimpleDotComGame.java
@@ -0,0 +1,23 @@
+package ex15SeaBattle;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и).");
+            }
+        }
+    }
+}
