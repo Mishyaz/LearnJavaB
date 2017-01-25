dd5275608316def02c484935183aeae8d5c670d4
Mishyaz <123qweASD>
2017-01-25 15:40:45 +0200

    add ex16

---
+++ src/ex16/Output.java
@@ -0,0 +1,22 @@
+package ex16;
+
+public class Output {
+    public static void main(String[] args) {
+        Output o = new Output();
+        o.go();
+    }
+
+    private void go() {
+        int y = 7;
+        for (int x = 1; x < 8; x++) {
+            y++;
+            if (x > 4) {
+                System.out.print(++y + " ");
+            }
+            if (y > 14) {
+                System.out.println(" x = " + x);
+                break;
+            }
+        }
+    }
+}
