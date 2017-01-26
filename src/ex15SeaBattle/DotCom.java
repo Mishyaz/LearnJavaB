d206c255f3e25c78b95c36ea968f31284919e2c1
Mishyaz <123qweASD>
2017-01-26 16:18:09 +0200

    add ex15

---
+++ src/ex15SeaBattle/DotCom.java
@@ -0,0 +1,25 @@
+package ex15SeaBattle;
+
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "Мимо";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "Потопил";
+            } else {
+                result = "Попал";
+            }
+        }
+        return result;
+    }
+}
